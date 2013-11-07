9a4045bca987f2c6a859ee1eab56af979f4319e4
java <java@formacion>
2013-11-07 16:04:41 +0100

    XML processing

--- springbatch/src/test/java/com/batch/springbatch/HelloWorldJobTestCase.java
+++ springbatch/src/test/java/com/batch/springbatch/HelloWorldJobTestCase.java
@@ -1,9 +1,14 @@
 package com.batch.springbatch;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +28,13 @@ public class HelloWorldJobTestCase {
 	
 	@Autowired
 	private Job helloWorldJob;
-	private JobParameters jobParameters = new JobParameters();
 
 	@Test
 	public void testLauchJob() throws Exception {
 	  logger.info("Running job");	
+      Map<String, JobParameter> params = new HashMap<String, JobParameter>();
+      params.put("date", new JobParameter(new Date(), false));
+	  JobParameters jobParameters = new JobParameters(params);
 	  jobLauncher.run(helloWorldJob, jobParameters);	
 	  logger.info("Job run");
 	}
